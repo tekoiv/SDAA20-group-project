ce080b8c1d62b451a033dd82f52d6b3463ad79a5
Topi Koivunen <tekoiv@utu.fi>
2020-10-05 16:21:18 +0000

    King made a bit better

--- src/GameLogic/King.java
+++ src/GameLogic/King.java
@@ -6,54 +6,16 @@ public class King extends ChessPiece {
         super(white);
     }
 
-    public boolean moves(Square start, Square finish, int x, int y){
-        // Moving forward 1 step either to up-left, forward or up-right
-        if ((start.get_y() - finish.get_y() == y && ((start.get_x() - finish.get_x() == y) || (start.get_x() - finish.get_x() == x ||(start.get_x() - finish.get_x() == -y))))) {
-            return true;
-        }
-        // Moving left or right
-        else if ((start.get_y() - finish.get_y() == x && ((start.get_x() - finish.get_x() == y) || (start.get_x() - finish.get_x() == -y)))) {
-        return true;
-        }
-        // Moving down-left, backwards, down-right
-        else if ((start.get_y() - finish.get_y() == -y && ((start.get_x() - finish.get_x() == y) || (start.get_x() - finish.get_x() == x ||(start.get_x() - finish.get_x() == -y))))) {
-            return true;
-        }
-        return false;
-    }
     @Override
     public boolean isAllowed(Board board, Square start, Square finish) {
         if (finish.getPiece() != null && finish.getPiece().isWhite() == this.isWhite()) {
-        return false;
+            return false;
         }
-        // Check for allowed moves
-        // White check, then black check
-        if (start.getPiece().isWhite()) {
-            if (finish.getPiece() == null) {
-                //White king moves
-                if(moves(start,finish,0,1)){
-                    return true;
-                }
-                // White king killing
-            } else {
-                if(moves(start,finish,0,1)){
-                return true;
-                }                 
-            }
-        } else {
-            if (finish.getPiece() == null) {
-                //Black king moves
-                if(moves(start,finish,0,1)){
-                return true;
-            }
-                // Black king killing
-            } else {
-                if(moves(start,finish,0,1)){
-                    return true;
-                }
-            }
-        }
-        return false;
+
+        int dist_x = Math.abs(start.get_x() - finish.get_x());
+        int dist_y = Math.abs(start.get_y() - finish.get_y());
+
+        return ((dist_x == 1 && dist_y == 0) || (dist_x == 0 && dist_y == 1) || (dist_x == 1 && dist_y == 1));
     }
 }
 
