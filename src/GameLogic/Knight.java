54fa178eee11b8763e36951d93a44b6a01d1de03
HelenaLahdesniemi <lahdesniemihelena@gmail.com>
2020-10-05 15:34:53 +0300

    Add finished Knight class

--- src/GameLogic/Knight.java
+++ src/GameLogic/Knight.java
@@ -2,67 +2,55 @@ package GameLogic;
 
 public class Knight extends ChessPiece {
     
-    private boolean startingMove = true;
-
     public Knight(boolean white){
         super(white);
     }
- 
 
-        @Override
-        public boolean isAllowed(Board board, Square start, Square finish) {
-          if (finish.getPiece() != null && finish.getPiece().isWhite() == this.isWhite()) {
-            return false;
+    public boolean moves (Square start, Square finish, int x, int y){
+        if ((start.get_y() - finish.get_y() == y || start.get_y() - finish.get_y() == -y) && ((start.get_x() - finish.get_x() == x)||(start.get_x() - finish.get_x() == -x))) {
+            return true;
           }
-          // Check for allowed moves
-          // White check, then black check
-          if (start.getPiece().isWhite()) {
-            if (finish.getPiece() == null) {
-              // Normal forward moves
-              if ((start.get_y() - finish.get_y() == 2 || start.get_y() - finish.get_y() == -2) && ((start.get_x() - finish.get_x() == 1)||(start.get_x() - finish.get_x() == -1))) {
-                this.startingMove = false;
-                return true;
-              }
-              if ((start.get_x() - finish.get_x() == 2 || start.get_x() - finish.get_x() == -2) && ((start.get_y() - finish.get_y() == 1)||(start.get_y() - finish.get_y() == -1))) {
-                this.startingMove = false;
-                return true;
-              }
-              
-              // Killing
-            } else {
-              if ((start.get_y() - finish.get_y() == 2 || start.get_y() - finish.get_y() == -2) && ((start.get_x() - finish.get_x() == 1)||(start.get_x() - finish.get_x() == -1)))  {
-                this.startingMove = false;
-                return true;
-              }
-              if ((start.get_x() - finish.get_x() == 2 || start.get_x() - finish.get_x() == -2) && ((start.get_y() - finish.get_y() == 1)||(start.get_y() - finish.get_y() == -1))) {
-                this.startingMove = false;
-                return true;
-              }
-            }
-          } else {
-            if (finish.getPiece() == null) {
-              // Normal forward moves
-              if ((start.get_y() - finish.get_y() == 2 || start.get_y() - finish.get_y() == -2) && ((start.get_x() - finish.get_x() == 1)||(start.get_x() - finish.get_x() == -1))) {
-                this.startingMove = false;
-                return true;
-              }
-              if ((start.get_x() - finish.get_x() == 2 || start.get_x() - finish.get_x() == -2) && ((start.get_y() - finish.get_y() == 1)||(start.get_y() - finish.get_y() == -1))) {
-                this.startingMove = false;
-                return true;
-              }
-              // Killing
-            } else {
-                if ((start.get_y() - finish.get_y() == 2 || start.get_y() - finish.get_y() == -2) && ((start.get_x() - finish.get_x() == 1)||(start.get_x() - finish.get_x() == -1))) {
-                    this.startingMove = false;
-                    return true;
-                  }
-                  if ((start.get_x() - finish.get_x() == 2 || start.get_x() - finish.get_x() == -2) && ((start.get_y() - finish.get_y() == 1)||(start.get_y() - finish.get_y() == -1))) {
-                    this.startingMove = false;
-                    return true;
-                  }
-            }
-          }
-          return false;
+        if ((start.get_x() - finish.get_x() == y || start.get_x() - finish.get_x() == -y) && ((start.get_y() - finish.get_y() == x)||(start.get_y() - finish.get_y() == -x))) {
+        return true;
         }
-      }
+        return false;
+    }
+
+    @Override
+    public boolean isAllowed(Board board, Square start, Square finish) {
+
+        if (finish.getPiece() != null && finish.getPiece().isWhite() == this.isWhite()) {
+        return false;
+        }
+
+        // Check for allowed moves
+        // White check, then black check
+        if (start.getPiece().isWhite()) {
+            if (finish.getPiece() == null) {
+                // White knight moves
+                if (moves(start, finish, 2, 1)){
+                    return true;
+                }
+                // White knight kills
+            } else {
+                if(moves(start,finish,2,1)){
+                    return true;
+                }
+            }
+        } else {
+            if (finish.getPiece() == null) {
+                // Black knight moves
+                if(moves(start,finish,2,1)){
+                return true;
+            }
+                // Black knight kills
+            } else {
+                if(moves(start,finish,2,1)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
       
