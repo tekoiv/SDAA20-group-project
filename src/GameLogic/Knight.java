83699ddc251c0283a0416e7cb2ec9f919ba024ed
HelenaLahdesniemi <lahdesniemihelena@gmail.com>
2020-10-05 17:29:58 +0300

    Improving the knight class

--- src/GameLogic/Knight.java
+++ src/GameLogic/Knight.java
@@ -6,51 +6,17 @@ public class Knight extends ChessPiece {
         super(white);
     }
 
-    public boolean moves (Square start, Square finish, int x, int y){
-        if ((start.get_y() - finish.get_y() == y || start.get_y() - finish.get_y() == -y) && ((start.get_x() - finish.get_x() == x)||(start.get_x() - finish.get_x() == -x))) {
-            return true;
-          }
-        else if ((start.get_x() - finish.get_x() == y || start.get_x() - finish.get_x() == -y) && ((start.get_y() - finish.get_y() == x)||(start.get_y() - finish.get_y() == -x))) {
-        return true;
-        }
-        return false;
-    }
-
     @Override
     public boolean isAllowed(Board board, Square start, Square finish) {
-
         if (finish.getPiece() != null && finish.getPiece().isWhite() == this.isWhite()) {
-        return false;
+            return false;
         }
 
-        // Check for allowed moves
-        // White check, then black check
-        if (start.getPiece().isWhite()) {
-            if (finish.getPiece() == null) {
-                // White knight moves
-                if (moves(start, finish, 2, 1)){
-                    return true;
-                }
-                // White knight killing
-            } else {
-                if(moves(start,finish,2,1)){
-                    return true;
-                }
-            }
-        } else {
-            if (finish.getPiece() == null) {
-                // Black knight moves
-                if(moves(start,finish,2,1)){
-                return true;
-            }
-                // Black knight killing
-            } else {
-                if(moves(start,finish,2,1)){
-                    return true;
-                }
-            }
-        }
-        return false;
+        int dist_x = Math.abs(start.get_x() - finish.get_x());
+        int dist_y = Math.abs(start.get_y() - finish.get_y());
+
+        return ((dist_x == 1 && dist_y == 2) || (dist_x == 2 && dist_y == 1));
     }
 }
+
       
